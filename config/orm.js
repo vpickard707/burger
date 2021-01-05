a301c1cd148f18c7adc818ec101b54a8f75941bb
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-04 22:05:37 -0800

    tst

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,19 @@
-// Import (require) connection.js into orm.js
+// Import (require) connection.js into orm.js so that the ORM can communicate with the database.
+var connection = require("./connection.js");
 // In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 // selectAll()
 // insertOne()
 // updateOne()
 // Export the ORM object in module.exports.
+
+var orm = {
+    //Select all function/query
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
