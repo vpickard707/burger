42ff8410cc784a2cfdf6214989be6bd8120d2adc
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-04 22:56:55 -0800

    set up routes for controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,4 +8,41 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-// Create the router for the app, and export the router at the end of your file.
+// Create the routes for the app, and export the routes at the end of your file
+
+// the JS object/data which is getting passed to view engine (handlebars)
+// Handlebars renders html file index.handlebars and renders the object burgers
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers", function (req, res) {
+  burger.insertOne(["burger_name"], [req.body.burger_name], function () {
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  // console.log("condition", condition);
+
+  burger.updateOne(
+    {
+      devoured: true,
+    },
+    condition,
+    function (data) {
+      res.redirect("/");
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
