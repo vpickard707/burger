330769fed2975890d691cd7e3532f8da73c33070
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-04 19:14:12 -0800

    added pseudocode and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+-- -- Write SQL queries this file that do the following:
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+
+USE burgers_db;
+
+ -- DEPARTMENT TABLE ----
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+  burger_name VARCHAR(30),
+--   salary DECIMAL,
+  devoured Boolean
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+---------
+-- Run the schema.sql and seeds.sql files into the mysql server from the command line
+----------------------
