a3958e28c66808029226861a340159e03d861957
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-04 22:09:56 -0800

    created insertOne method for mySQL

--- config/orm.js
+++ config/orm.js
@@ -17,3 +17,24 @@ var orm = {
         cb(result);
       });
     },
+//Create insertOne() function/query
+create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
